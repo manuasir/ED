d18854b7b49cf903c0e292ccd9d891ee5172c71b
Manuel <manu.asi2@gmail.com>
2017-10-28 23:27:31 +0200

    seteando palabras en matriz dispersa

--- TDA/practica/include/sopa_letras.h
+++ TDA/practica/include/sopa_letras.h
@@ -25,9 +25,49 @@ class Sopa_letras{
 			    //cout << line << endl;
 			    int i, j;
 			    string dir,palabra;
+			   
 			    if (!(iss >> i >> j >> dir >> palabra)) { break; } // error
-			    //cout << a << b << endl;
-			    cout << dir << endl;
+			    char * palabra_char = new char[palabra.length() + 1];
+				strcpy(palabra_char, palabra.c_str());
+			    cout << "palabra string " << palabra << endl;
+			    if(dir == "hi") // horizontal izquierda
+				    for (unsigned int index=0;index<palabra.length();index++,j++){
+				    	cout << "llamando a setear " << endl;
+	    				sopa.matriz.Set(i,j,palabra_char[index]);
+				    }
+			    else if (dir == "hd"){ // horizontal derecha
+				    	cout << "llamando a setear " << palabra.length() << palabra << endl;
+
+			    	for (unsigned int index=0;index<palabra.length();index++,j--){
+				    	cout << "llamando a setear " << palabra_char[index] << endl;
+
+	    				sopa.matriz.Set(i,j,palabra_char[index]);
+			    	}
+			    }
+    			else if (dir == "vu") // vertical arriba
+			    	for (unsigned int index=0;index<palabra.length();index++,i--){
+				    	cout << "llamando a setear " << endl;
+	    				sopa.matriz.Set(i,j,palabra_char[index]);
+			    	}
+    			else if (dir == "vd") // vertical abajo
+			    	for (unsigned int index=0;index<palabra.length();index++,i++){
+						cout << "llamando a setear " << endl;
+	    				sopa.matriz.Set(i,j,palabra_char[index]);
+			    	}
+    			else if (dir == "dd") // diagonal abajo derecha
+			    	for (unsigned int index=0;index<palabra.length();index++,i++,j++){
+				    	cout << "llamando a setear " << endl;
+	    				sopa.matriz.Set(i,j,palabra_char[index]);
+			    	}
+    			else if (dir == "di") // diagonal abajo izquierda
+			    	for (unsigned int index=0;index<palabra.length();index++,i++,j--){
+			    		cout << "llamando a setear " << endl;
+	    				sopa.matriz.Set(i,j,palabra_char[index]);
+			    	}
+    			else
+    				cout << "NO COINCIDE" << dir << endl;
+				delete[] palabra_char;
+
 			}
 		    return is;
 		}
