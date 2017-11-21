2e86ec9cddb8e7b99cb65f37e14cad597f4837eb
manuasir <manu.asi2@gmail.com>
2017-11-21 03:26:58 +0100

    fixing indent

--- iteradores/src/pruebadiccionario.cpp
+++ iteradores/src/pruebadiccionario.cpp
@@ -7,13 +7,13 @@ using namespace std;
 ostream & operator<<(ostream & os, const pair<string,string> & p){
   os<<p.first<<";"<<p.second<<endl;
   return os;
-}  
+}
 void ImprimirDiccionario(Diccionario &D){
   Diccionario::iterator it;
   for (it=D.begin(); it!=D.end();++it){
     cout<<*it<<endl;
   }
-}  
+}
 int main(int argc, char * argv[]){
 
   if (argc!=2){
@@ -26,19 +26,19 @@ int main(int argc, char * argv[]){
     cout<<"No puedo abrir el fichero "<<argv[1]<<endl;
     return 0;
    }
-   
+
    Diccionario T;
    f>>T; //Cargamos en memoria el diccionario
    cout<<T;
    string a;
-   
-   
+
+
    cout<<"********Dime un tema:";
    getline(cin,a);
    cout<<endl<<endl<<"************Palabras con ese tema:"<<endl;
    Diccionario Dtema = T.ObtainPalabrasconDeficionContiene(a);
-   
-   
+
+
    ImprimirDiccionario(Dtema);
-      
+
 }
