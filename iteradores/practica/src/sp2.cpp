b02ecd3918895b7bf7b30b6b20c60c202ff6d4bd
manuasir <manu.asi2@gmail.com>
2017-11-30 03:28:46 +0100

    terminando funcionalidad basica :+1:

--- iteradores/practica/src/sp2.cpp
+++ iteradores/practica/src/sp2.cpp
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <fstream>
 #include <list>
+#include <stdlib.h>
 #include "../include/sopa_letras.h"
 #include "../include/diccionario.h"
 
@@ -9,15 +10,14 @@ list<string> extraerPalabras(Diccionario &D){
   list<string> palabras;
   for (it=D.begin(); it!=D.end();++it){
     for(int i = 0;i<int((*it).second.size());i++){
-      cout<<"una palabra: " << (*it).first<<endl;
-    //  palabras.insert( (*it).first);
+      palabras.push_back( (*it).first);
     }
   }
   return palabras;
 }
 
 int main(int argc, char * argv[]){
-
+  srand (time(NULL));
   if (argc!=4){
     cout<<"Dime el nombre del fichero con las palabras de la sopa de letras, el tema y el número máximo de palabras"<<endl;
     return 0;
@@ -30,20 +30,37 @@ int main(int argc, char * argv[]){
 
   Sopa_letras Sl;
   Diccionario T;
-  cout<<"Dime una palabra: ";
-  string word;
-  cin>>word;
+  string word(argv[2]);
+  cout << "daword " << word << endl;
+
   f>>T; //Cargamos en memoria el diccionario
 
   Diccionario Dtema = T.ObtainPalabrasconDeficionContiene(word);
   Diccionario::iterator it;
   list<string> palabras = extraerPalabras(Dtema);
-
+  list<string>::iterator listIterator;
+  //int fila,columna,maxPalabras;
+  for(listIterator=palabras.begin();listIterator!=palabras.end();++listIterator){
+    cout << "una palabra: " << (*listIterator) << endl;
+    string dir;
+    int randomDir = rand()%5;
+    switch(randomDir){
+      case 0: dir = "vd"; break;
+      case 1: dir = "vu"; break;
+      case 2: dir = "hd"; break;
+      case 3: dir = "hi"; break;
+      case 4: dir = "dd"; break;
+      case 5: dir = "di"; break;
+    }
+    Sl.addPalabra(rand()%20,rand()%20,(*listIterator),dir);
+  }
   //El usuario ahora tiene que ir descubriendo las palabras
   //en la sopa de letras
   while (Sl.NumPalabras()!=0){
     cout<<Sl<<endl;
-
+    string pal;
+    cout<<"Dime una palabra :";
+    cin>>pal;
     cout<<"Dime la fila :";
     int row;
     cin>>row;
@@ -55,12 +72,12 @@ int main(int argc, char * argv[]){
 
     string direccion;
     cin>>direccion;
-    if (!Sl.Comprobar_Palabra(word,row,col,direccion)){//Esta?
-      cout<<"La palabra "<<word << " no esta"<<endl;
+    if (!Sl.Comprobar_Palabra(pal,row,col,direccion)){//Esta?
+      cout<<"La palabra "<<pal << " no esta"<<endl;
     } else{
       cout << "bien!" << endl;
       //La pone en negrita. Ademas la pone dentro de la lista de palabras descubiertas.
-      Sl.Poner_Acertada(word,row,col,direccion);
+      Sl.Poner_Acertada(pal,row,col,direccion);
     }
   }
 }
