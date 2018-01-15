6e28c12dff32a4ab069b86dbc4e6d132b80970f9
manuasir <manu.asi2@gmail.com>
2018-01-15 22:31:32 +0100

    metodos de info_nodo

--- ejercicios/binaryTree/binaryTree.cpp
+++ ejercicios/binaryTree/binaryTree.cpp
@@ -27,7 +27,7 @@ void ArbolBinario<T>::BorrarInfo(info_nodo * & nodo){
 template <class T>
 int ArbolBinario<T>::NumeroNodos(const info_nodo * nodo)const{
   if(nodo == 0)
-    return 0;
+  return 0;
   else {
     return NumeroNodos(nodo->hijoIzq) + NumeroNodos(nodo->hijoDer)+1;
   }
@@ -36,13 +36,79 @@ int ArbolBinario<T>::NumeroNodos(const info_nodo * nodo)const{
 template <class T>
 bool ArbolBinario<T>::Iguales(const info_nodo * nodo1, const info_nodo * nodo2)const{
   if(nodo1 == 0  and nodo2 == 0)
-    return true;
+  return true;
   if(nodo1 == 0  or nodo2 == 0)
-    return false;
+  return false;
   else {
     if (nodo1->etiqueta == nodo2->etiqueta) {
-       Iguales(nodo1->hijoIzq,nodo2->hijoIzq) && (nodo1->hijoDer,nodo2->hijoDer);
+      Iguales(nodo1->hijoIzq,nodo2->hijoIzq) && Iguales(nodo1->hijoDer,nodo2->hijoDer);
     } else
-      return false;
+    return false;
   }
+}
+
+template <class T>
+void ArbolBinario<T>::InsertarHijoIzquierda(info_nodo * nodo, info_nodo * & sub){
+  info_nodo * aux = nodo->hijoIzq;
+  if (sub != 0) {
+    nodo->hijoIzq = sub;
+    BorrarInfo(aux);
+    nodo->hijoIzq->padre = nodo;
+    sub = 0;
+  } else {
+    nodo->hijoIzq = 0;
+    BorrarInfo(aux);
+  }
+}
+
+template <class T>
+void ArbolBinario<T>::InsertarHijoIzquierda(info_nodo * nodo, const T & et){
+  info_nodo * aux = new info_nodo(et);
+  InsertarHijoIzquierda(nodo,aux);
+}
+
+template <class T>
+void ArbolBinario<T>::InsertarHijoDerecha(info_nodo * nodo, info_nodo * & sub) {
+  info_nodo * aux = nodo->hijoDer;
+  if (sub != 0) {
+    nodo->hijoDer = sub;
+    BorrarInfo(aux);
+    nodo->hijoDer->padre = nodo;
+    sub = 0;
+  } else {
+    nodo->hijoDer = 0;
+    BorrarInfo(aux);
+  }
+}
+
+template <class T>
+void ArbolBinario<T>::InsertarHijoDerecha(info_nodo * nodo, const T & et) {
+  info_nodo * aux = new info_nodo(et);
+  InsertarHijoDerecha(nodo,aux);
+}
+
+template <class T>
+void ArbolBinario<T>::PodarHijoIzquierda(info_nodo * nodo) {
+  if(nodo->hijoIzq != 0) {
+    BorrarInfo(nodo->hijoIzq);
+    nodo->hijoIzq = 0;
+  }
+}
+
+template <class T>
+void ArbolBinario<T>::PodarHijoDerecha(info_nodo * nodo) {
+  if(nodo->hijoDer != 0) {
+    BorrarInfo(nodo->hijoDer);
+    nodo->hijoDer = 0;
+  }
+}
+
+template <class T>
+typename ArbolBinario<T>::info_nodo ArbolBinario<T>::PodarHijoIzquierda_GetSubTree(info_nodo * nodo) {
+  info_nodo * aux = nodo->hijoIzq;
+  nodo->hijoIzq = 0;
+  if(aux != 0) {
+    aux->padre = 0;
+  }
+  return aux;
 }
--- ejercicios/binaryTree/binaryTree.h
+++ ejercicios/binaryTree/binaryTree.h
@@ -21,13 +21,13 @@ private:
   void BorrarInfo(info_nodo * & nodo);
   int NumeroNodos(const info_nodo * nodo)const;
   bool Iguales(const info_nodo * nodo1, const info_nodo * nodo2)const;
-  void InsertarHijoIzquierda(info_nodo * padre, info_nodo * hijo);
-  void InsertarHijoIzquierda(info_nodo * padre, const T & et);
-  void InsertarHijoDerecha(info_nodo * padre, info_nodo * hijo);
-  void InsertarHijoDerecha(info_nodo * padre, const T & et);
+  void InsertarHijoIzquierda(info_nodo * nodo, info_nodo * & sub);
+  void InsertarHijoIzquierda(info_nodo * nodo, const T & et);
+  void InsertarHijoDerecha(info_nodo * nodo, info_nodo * & sub);
+  void InsertarHijoDerecha(info_nodo * nodo, const T & et);
   void PodarHijoIzquierda(info_nodo * nodo);
   void PodarHijoDerecha(info_nodo * nodo);
-  info_nodo * PodarHijoIzquierda_GetSubTree(info_nodo * nodo);
+  info_nodo PodarHijoIzquierda_GetSubTree(info_nodo * nodo);
   info_nodo * PodarHijoDerecha_GetSubTree(info_nodo * nodo);
   void RecorridoPostorden(ostream & os, const info_nodo * nodo)const;
   void RecorridoPreorden(ostream & os, const info_nodo * nodo)const;
